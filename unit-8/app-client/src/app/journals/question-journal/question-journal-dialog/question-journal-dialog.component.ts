6350453743f944e2a8b683a64109914a1cc44e84
Anisimova Anastasia <anisimova-nastya2@bk.ru>
2019-09-08 11:14:15 +0700

    add form builder array

--- unit-8/app-client/src/app/journals/question-journal/question-journal-dialog/question-journal-dialog.component.ts
+++ unit-8/app-client/src/app/journals/question-journal/question-journal-dialog/question-journal-dialog.component.ts
@@ -1,6 +1,6 @@
 import {ChangeDetectionStrategy, Component, Inject, OnInit, ViewChild, ViewChildren} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material';
-import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {QuestionJournalItem} from 'src/app/model/question-journal-item.model';
 import {QuestionJournalItemAnswer} from '../../../model/question-journal-item-answer.model';
 import {AnswersListComponent} from '../components/answers-list/answers-list.component';
@@ -52,22 +52,19 @@ export class QuestionJournalDialogComponent implements OnInit {
   }
 
   addAnswer() {
-    this.answers.push(
-      {
-        answerText: this.answerForm.get('answerText').value,
-        isCorrect: this.answerForm.get('isCorrect').value
-      }
+    (this.dialogForm.controls[`arr`] as FormArray).push(
+      new FormGroup({
+          answerText: new FormControl('', Validators.required),
+          isCorrect: new FormControl(false, Validators.required)
+        }
+      )
     );
-    this.clear();
   }
 
-  private clear() {
-    this.answerForm.get('answerText').setValue('');
-    this.answerForm.get('isCorrect').setValue(false);
-  }
 
-  removeItem(item: QuestionJournalItemAnswer) {
-    this.answers = this.answers.filter(val => val !== item);
+  removeItem(index: number) {
+    console.log(index);
+    (this.dialogForm.get('arr') as FormArray).removeAt(index);
   }
 
   public checkAnswers(isInvalid: boolean) {
@@ -95,33 +92,35 @@ export class QuestionJournalDialogComponent implements OnInit {
   }
 
   private fillForm() {
+    const answersArr: FormGroup[] = this.answers.map(ans =>
+      new FormGroup({
+          answerText: new FormControl(ans.answerText, Validators.required),
+          isCorrect: new FormControl(ans.isCorrect, Validators.required)
+        }
+      )
+    );
+
     this.dialogForm = this.fb.group({
       description: new FormControl(this.description, Validators.required),
-      demoArray: this.fb.array([]),
+      isCorrect: new FormControl(),
+      arr: this.fb.array(answersArr),
     });
 
-    // this.answerForm = this.fb.group({
-    //   answerText: new FormControl('', Validators.required),
-    //   isCorrect: new FormControl(),
-    // });
   }
 
 
   onDialogSubmit() {
+    this.answers = this.dialogForm.get('arr').value;
     console.log(this.answers);
-    // this.answers = this.child.getList();
-    const hasCorrect = this.answers.filter(answer => answer.isCorrect);
-    // if (!hasCorrect) {
-    //   this.showErrorMessage = true;
-    // } else {
-    //   const newValues: QuestionJournalDialogResult = {
-    //     name: this.dialogForm.get('description').value,
-    //     answersCount: this.dialogForm.get('answersCount').value,
-    //     answers: [],
-    //   };
+
+    // const newValues: QuestionJournalDialogResult = {
+    //   name: this.dialogForm.get('description').value,
+    //   answersCount: this.dialogForm.get('answersCount').value,
+    //   answers: this.answers,
+    // };
     //
-    //   this.dialogRef.close(newValues);
-    // }
+    // this.dialogRef.close(newValues);
+
 
   }
 
--- unit-8/app-server/src/main/java/com/github/siberianintegrationsystems/restApp/controller/dto/QuestionsItemDTO.java
+++ unit-8/app-server/src/main/java/com/github/siberianintegrationsystems/restApp/controller/dto/QuestionsItemDTO.java
@@ -1,12 +1,17 @@
 package com.github.siberianintegrationsystems.restApp.controller.dto;
 
+
+import java.util.List;
+
 public class QuestionsItemDTO extends JournalItemDTO {
     public String name;
     public int answersCount;
+    public List<Answer> answers;
 
-    public QuestionsItemDTO(String id, String name, int answersCount) {
+    public QuestionsItemDTO(String id, String name, int answersCount, List<Answer> answers) {
         this.id = id;
         this.name = name;
+        this.answers = answers;
         this.answersCount = answersCount;
     }
 }
